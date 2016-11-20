5b45fb495944bb82f42fa4b87cb4c5c3043d12b9
lucyi12 <2602458273@qq.com>
2016-11-20 22:46:26 +0800

    根据鼠标坐标获得box坐标getBoxAtPixel(x, y)

---
+++ getBoxAtPixel.py
@@ -0,0 +1,8 @@
+def getBoxAtPixel(x, y):
+    for boxx in range(BOARDWIDTH):
+        for boxy in range(BOARDHEIGHT):
+            left, top = leftTopCoordsOfBox(boxx, boxy)
+            boxRect = pygame.Rect(left, top, BOXSIZE, BOXSIZE)
+            if boxRect.collidepoint(x, y):
+                return (boxx, boxy)
+    return (None, None)
