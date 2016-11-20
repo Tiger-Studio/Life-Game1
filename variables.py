f03e4b1a016c96abc29f0efc8b610d987021bb5a
ssssswwyy <942861699@qq.com>
2016-11-20 09:08:07 -0600

    变量整合

---
+++ variables.py
@@ -0,0 +1,34 @@
+FPS = 30 # frames per second, the general speed of the program
+WINDOWWIDTH = 640 # size of window's width in pixels
+WINDOWHEIGHT = 480 # size of windows' height in pixels
+REVEALSPEED = 8 # speed boxes' sliding reveals and covers
+BOXSIZE = 13 # size of box height & width in pixels
+GAPSIZE = 3 # size of gap between boxes in pixels
+BOARDWIDTH = 30 # number of columns of icons
+BOARDHEIGHT = 20 # number of rows of icons
+XMARGIN = int((WINDOWWIDTH - (BOARDWIDTH * (BOXSIZE + GAPSIZE))) / 2)
+YMARGIN = int((WINDOWHEIGHT - (BOARDHEIGHT * (BOXSIZE + GAPSIZE))) / 2)
+
+DIE=0
+LIVE=1
+
+#            R    G    B
+GRAY     = (100, 100, 100)
+NAVYBLUE = ( 60,  60, 100)
+WHITE    = (255, 255, 255)
+RED      = (255,   0,   0)
+GREEN    = (  0, 255,   0)
+BLUE     = (  0,   0, 255)
+YELLOW   = (255, 255,   0)
+ORANGE   = (255, 128,   0)
+PURPLE   = (255,   0, 255)
+CYAN     = (  0, 255, 255)
+
+BGCOLOR = NAVYBLUE
+LIGHTBGCOLOR = GRAY
+BOXCOLOR = WHITE
+HIGHLIGHTCOLOR = BLUE
+
+
+CIRCLE= 'circle'
+RECT= 'rect'
