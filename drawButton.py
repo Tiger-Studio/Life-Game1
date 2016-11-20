f7c231ad0ca3dc44dab01f14ce23d4733819dac0
QYY2016 <1416146233@qq.com>
2016-11-20 23:37:59 +0800

    实现了画按钮
    其中有getButtonAtPixel(x, y)，根据鼠标坐标得到buttonNum；还有leftTopCoordsOfButton(buttonNum)，根据buttonNum得到真实button的left边和top边位置

---
+++ drawButton.py
@@ -0,0 +1,30 @@
+def leftTopCoordsOfButton(buttonNum):
+    # Convert board coordinates to pixel coordinates
+    left = WINDOWWIDTH-XMARGIN-BUTTONWIDTH*(buttonNum+1)-BUTTONGAP*buttonNum
+    top = WINDOWHEIGHT
+    return (left, top)
+
+def getButtonAtPixel(x, y):
+    for buttonNum in range(BUTTONCOUNT):
+        left, top = leftTopCoordsOfButton(buttonNum)
+        boxRect = pygame.Rect(left, top, BUTTONWIDTH, BUTTONHEIGHT)
+        if boxRect.collidepoint(x, y):
+            return buttonNum
+    return None
+
+def drawButton(buttonNum):
+    left,top=leftTopCoordsOfButton(buttonNum)
+    if buttonNum==0:
+        basicFont = pygame.font.SysFont(None, 35)  
+        text = basicFont.render(" next ", True, BLACK, GRAY) 
+        textRect = text.get_rect()     
+        textRect.centerx = left+BUTTONWIDTH/2
+        textRect.centery = top+BUTTONHEIGHT/2  
+        DISPLAYSURF.blit(text, textRect)
+    if buttonNum==1:
+        basicFont = pygame.font.SysFont(None, 35)  
+        text = basicFont.render(" reset ", True, BLACK, GRAY) 
+        textRect = text.get_rect()     
+        textRect.centerx = left+BUTTONWIDTH/2
+        textRect.centery = top+BUTTONHEIGHT/2  
+        DISPLAYSURF.blit(text, textRect) 
