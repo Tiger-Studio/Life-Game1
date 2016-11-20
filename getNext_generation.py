fc3f1c1a027dbe5a765d23c7e85e90097fe546cd
YangCHen456 <903149342@qq.com>
2016-11-20 23:52:57 +0800

    更改下一代算法

--- getNext_generation.py
+++ getNext_generation.py
@@ -8,6 +8,8 @@ def getNext_generation(board):
     for boxx in range(BOARDWIDTH):
         for boxy in range(BOARDHEIGHT):
             lifeCount=0
+            if board[boxx][boxy]==LIVE:
+                lifeCount=lifeCount+1
             if (boxx-1)>=0 and (boxy-1)>=0 and board[boxx-1][boxy-1]==LIVE:
                 lifeCount=lifeCount+1
             if (boxx-1)>=0 and board[boxx-1][boxy]==True:
